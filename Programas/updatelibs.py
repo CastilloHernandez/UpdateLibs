47ba68c58685fa093763ee4b4d309865f0b87f2c
Emmanuel Castillo <emmanuel156@gmail.com>
2016-12-02 16:44:52 -0600

    Se corrije error copiaba librerias de BIN\RELEASE sobreescribiendo las de BIN\DEBUG

--- Programas/updatelibs.py
+++ Programas/updatelibs.py
@@ -1,6 +1,6 @@
 import os
 #import mimetypes
-#import hashlib
+import hashlib
 #import urllib2
 import argparse
 import shutil
@@ -9,6 +9,25 @@ import shutil
 #	opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
 #	request = opener.open(url)
 #	return request.url
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
 
 def BuscarLibrerias(dir):
 	r=[]
@@ -24,7 +43,7 @@ def BuscarBinDebug(dir):
 	for root, dirs, files in os.walk(dir):
 		for d in dirs:
 			if os.path.basename(root).upper() == 'BIN' and d.upper() == 'DEBUG':
-				r.append(root)
+				r.append(os.path.join(root,d))
 	return sorted(r)
 
 parser = argparse.ArgumentParser(prog='updatelibs')
@@ -42,9 +61,18 @@ for directorio in directorios:
 	for d in BuscarBinDebug(directorio):
 		print 'Directorio ' + d
 		for f in BuscarLibrerias(d):
-			libdir = os.path.join( os.path.dirname(os.path.dirname(os.path.dirname(f))),'lib')
+			libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(f))),'lib')
 			if not os.path.isdir(libdir):
 				os.makedirs(libdir)
-			print 'Copiando ' + os.path.basename(f)
-			shutil.copyfile(f, os.path.join(libdir, os.path.basename(f)))
+			dest=os.path.join(libdir, os.path.basename(f))
+			if os.path.exists(dest):
+				hashdest=hash_file(dest)
+				if hashdest == hash_file(f):
+					print '  ' + os.path.basename(f)
+				else:
+					print '* ' + os.path.basename(f)
+					shutil.copyfile(f, dest)
+			else:
+				print '+ ' + os.path.basename(f)
+				shutil.copyfile(f, dest)
 

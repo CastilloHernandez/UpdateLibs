bff0457460a43c7eb9af7e29cf018001ac8604b3
Emmanuel Castillo <emmanuel156@gmail.com>
2017-01-21 13:40:03 -0600

    Limpieza de codigo

--- Programas/updatelibs.py
+++ Programas/updatelibs.py
@@ -1,32 +1,17 @@
 import os
-#import mimetypes
 import hashlib
-#import urllib2
 import argparse
 import shutil
 
-#def get_redirected_url(url):
-#	opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
-#	request = opener.open(url)
-#	return request.url
 def hash_file(filename):
-   """"This function returns the SHA-1 hash
-   of the file passed into it"""
+   # Calcula hash SHA-1 de un archivo en bloques de 1024 bytes 
 
-   # make a hash object
    h = hashlib.sha1()
-
-   # open file for reading in binary mode
    with open(filename,'rb') as file:
-
-       # loop till the end of the file
        chunk = 0
        while chunk != b'':
-           # read only 1024 bytes at a time
            chunk = file.read(1024)
            h.update(chunk)
-
-   # return the hex representation of digest
    return h.hexdigest()
 
 def BuscarLibrerias(dir):
@@ -35,9 +20,9 @@ def BuscarLibrerias(dir):
 		for file in files:
 			f,e = os.path.splitext(file)
 			if e.upper() == '.DLL':
-				#print str(os.path.join(root,file))
 				r.append(str(os.path.join(root,file)))
 	return sorted(r)	
+	
 def BuscarBinDebug(dir):
 	r=[]
 	for root, dirs, files in os.walk(dir):
@@ -47,7 +32,6 @@ def BuscarBinDebug(dir):
 	return sorted(r)
 
 parser = argparse.ArgumentParser(prog='updatelibs')
-parser.add_argument('-branch',default='trunk')
 parser.add_argument('directorio',nargs='*')
 opt = parser.parse_args()
 
@@ -59,7 +43,7 @@ else:
 
 for directorio in directorios:
 	for d in BuscarBinDebug(directorio):
-		print 'Directorio ' + d
+		print '> ' + d
 		for f in BuscarLibrerias(d):
 			libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(f))),'lib')
 			if not os.path.isdir(libdir):
